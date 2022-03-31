cb7f8f03ab8573ecaebb8272a9fda7de208f06ba
eMKa-12200420 <12200420@bsi.ac.id>
2022-03-31 09:41:24 +0700

    Web 3 Pert 3

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -17,11 +17,13 @@ class Home extends CI_Controller
             $data['user'] = $user['nama'];
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         } else {
             $data['user'] = 'Pengunjung';
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         }
     }
@@ -47,6 +49,7 @@ class Home extends CI_Controller
         }
         $this->load->view('templates/templates-user/header', $data);
         $this->load->view('buku/detail-buku', $data);
+        $this->load->view('templates/templates-user/modal');
         $this->load->view('templates/templates-user/footer');
     }
 }
