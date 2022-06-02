d2b282089c5c713c6dc07d78194c2b258e88d477
eMKa-12200420 <12200420@bsi.ac.id>
2022-06-02 09:15:25 +0700

    Web 3 Pert 9

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,7 +6,6 @@ class Admin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model(['ModelBuku', 'ModelUser', 'ModelBooking']);
         cek_login();
         cek_user();
     }
@@ -19,17 +18,19 @@ class Admin extends CI_Controller
         $data['buku'] = $this->ModelBuku->getLimitBuku()->result_array();
 
         //mengupdate stok dan dibooking pada tabel buku
-        $detail = $this->db->query("SELECT*FROM booking,booking_detail, buku WHERE booking.id_booking=booking_detail.id_booking and booking_detail.id_buku=buku.id")->result_array();
+        $detail = $this->db->query("SELECT*FROM booking,booking_detail WHERE DAY(curdate()) <DAY(batas_ambil) AND booking.id_booking=booking_detail.id_booking")->result_array();
         foreach ($detail as $key) {
             $id_buku = $key['id_buku'];
             $batas = $key['tgl_booking'];
             $tglawal = date_create($batas);
             $tglskrg = date_create();
+
             $beda = date_diff($tglawal, $tglskrg);
             if ($beda->days > 2) {
                 $this->db->query("UPDATE buku SET stok=stok+1, dibooking=dibooking-1 WHERE id='$id_buku'");
             }
         }
+
         //menghapus otomatis data booking yang sudah lewat dari 2 hari
         $booking = $this->ModelBooking->getData('booking');
         if (!empty($booking)) {
@@ -39,6 +40,7 @@ class Admin extends CI_Controller
                 $tglawal = date_create($tglbooking);
                 $tglskrg = date_create();
                 $beda = date_diff($tglawal, $tglskrg);
+
                 if ($beda->days > 2) {
                     $this->db->query("DELETE FROM booking WHERE id_booking='$id_booking'");
                     $this->db->query("DELETE FROM booking_detail WHERE id_booking='$id_booking'");
--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -28,4 +28,32 @@ class Laporan extends CI_Controller
         $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
         $this->load->view('buku/laporan_print_buku', $data);
     }
+
+    public function laporan_buku_pdf()
+    {
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+
+        $sroot = $_SERVER['DOCUMENT_ROOT'];
+        include $sroot . "/pustaka-booking/application/third_party/dompdf/autoload.inc.php";
+        $dompdf = new Dompdf\Dompdf();
+
+        $this->load->view('buku/laporan_pdf_buku', $data);
+        $paper_size = 'A4'; // ukuran kertas
+        $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+        $html = $this->output->get_output();
+
+        $dompdf->set_paper($paper_size, $orientation);
+        $dompdf->load_html($html);
+        $dompdf->render();
+        $dompdf->stream("laporan_data_buku.pdf", array('Attachment' => 0));
+    }
+
+    public function export_excel()
+    {
+        $data = array(
+            'title' => 'Laporan Buku',
+            'buku' => $this->ModelBuku->getBuku()->result_array()
+        );
+        $this->load->view('buku/export_excel_buku', $data);
+    }
 }
